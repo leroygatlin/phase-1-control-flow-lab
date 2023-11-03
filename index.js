acb0a5261366c9298efd75936a0b75cc7692454d
leroygatlin <leroygatlin@gmail.com>
2023-11-02 19:02:54 -0700

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 399){
+    return 'This one is on me!'
+  } 
+    else if (feet <= 2000) { 
+    return 'That will be twenty bucks.'
+  } 
+    else if (feet <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+    else if (feet >= 2501){
+    return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+ const message = destination === 'NYC' ? ('Ok, sounds good.') : ('No go.')
+ return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  switch(generous){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+      break;
+      default:
+      return "Bye."
+  }
 }
